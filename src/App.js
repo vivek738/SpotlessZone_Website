91956f1f00e9750f7292ac1f4120790a50e0a499
punamoli77 <punamoli189@gmail.com>
2022-05-12 20:18:08 +0545

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
 import './Components/Products/AddProduct'
+import { BrowserRouter } from 'react-router-dom';
 import Addproduct from './Components/Products/AddProduct';
+import { Container } from './Routes/Container';
 function App() {
   return (
     <>
-    <Addproduct></Addproduct></>
+    <BrowserRouter>
+    <Container/>
+
+    
+    </BrowserRouter>
+    </>
+    
+    
     
     
   )}
---
+++ src/Routes/Container.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+export const Container = () => {
+  return (
+
+    <>
+    <Routes>
+
+
+
+
+    </Routes>
+    
+    </>
+    
+  )
+}
