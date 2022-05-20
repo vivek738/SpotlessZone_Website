cbc4b8bfe922c166ff20a64f7ce4be68f63b3951
Ramesh Pathak <cpathakramesh123@gmail.com>
2022-05-20 18:45:52 +0545

    adding parseJWT

--- src/Routes/Container.js
+++ src/Routes/Container.js
@@ -3,19 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import EmailVerify from "../Components/EmailVerify";
 import Homepage from "../Components/Homepage/Homepage";
 import Login from "../Components/Login";
-import Addproduct from "../Components/Products/AddProduct";
 import Update from "../Components/Products/UpdateProduct";
 import EditProfile from "../Components/Profile/EditProfile";
 import SignUp from "../Components/SignUp";
-
+import { parseJwt } from "../utils/parseJwt";
 export const Container = () => {
   //in login branch
-  // const user = localStorage.getItem("token");
+  const user = localStorage.getItem("token");
+  const decodeUser = parseJwt(user)
+  console.log(decodeUser.user)
   return (
     <>
       <Routes>
         <Route path="/" exact element={<Homepage />} />
-        <Route path="/addProduct" element={<Addproduct />}></Route>
+        {
+          decodeUser.user?.role === "admin"?
+          <Route path="/edit-profile" element={<EditProfile />}></Route>
+          :
+          <></>
+        }
         {/* {user && (
           
         )} */}
@@ -28,7 +34,6 @@ export const Container = () => {
         />
         <Route path='/update-product' element={<Update/>}></Route>
         <Route path="/edit-profile" element={<EditProfile />}></Route>
-
       </Routes>
     </>
   );
