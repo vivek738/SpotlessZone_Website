4551873f6b1a1d66f4ca86b066decea75e878445
unknown <buggatichannel6@gmail.com>
2022-06-03 11:26:21 +0545

    Add Contact Page

--- src/Components/Contactus/Contactus.js
+++ src/Components/Contactus/Contactus.js
@@ -1,52 +1,84 @@
 import React from "react";
 import './Contactus.css';
-import pic from './images/faq.jpg';
+import pic from '../../Images/faq.jpg';
 import { MDBContainer } from 'mdb-react-ui-kit';
+import gmail from '../../Images/gmail.png';
+import placeholder from '../../Images/placeholder.png';
+import phonecall from '../../Images/phonecall.png';
 
 
-const Contactus = ()=>{
-    return(
+
+const Contactus = () => {
+  return (
     <>
 
-    <div className="image">
-        <img src= {pic}></img>
-    </div>
+      <div className="image">
+        <img src={pic} alt=""></img>
+      </div>
 
-    <div className="middle">
+      <div className="middle">
         <h4 className="heading mt-4">Contact Us</h4>
         <p>We are here to help and answer any question you might have. we look forward to hearing from you.</p>
-    </div>
-    <MDBContainer>
-    <div className=" col-md-5 mx-auto">
+      </div>
+      <MDBContainer>
+        <div className=" col-md-5 mx-auto">
 
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"></input>
-  </div>
+          <form>
+            <div className="mb-3">
+              <input type="email" placeholder="Password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+              <input type="password" placeholder="Password" class="form-control" id="exampleInputPassword1"></input>
+            </div>
 
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"></input>
-  </div>
+            <div className="mb-3">
+              <input type="password" placeholder="Password" class="form-control" id="exampleInputPassword1"></input>
+            </div>
 
-  
 
-  
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
-    </MDBContainer>
 
-    
-    
+
+            <button type="submit" className="submit">Submit</button>
+          </form>
+        </div>
+
+        <div className="last me">
+          <div className="row d-flex mt-5">
+
+            <div className="col-md-4">
+              <img src={phonecall} alt=""></img>
+              <h5>Phone</h5>
+              <p>940489374</p>
+
+            </div>
+
+            <div className="col-md-4">
+              <img src={gmail} alt="" />
+
+              <h5>E-mail</h5>
+              <p>dipikabogat6@gmail.com</p>
+
+
+            </div>
+
+            <div className="col-md-4">
+              <img src={placeholder} alt=""></img>
+
+              <h5>Location</h5>
+              <p>51 Street, Newyork City, NYPD
+              </p>
+
+
+            </div>
+          </div>
+        </div>
+      </MDBContainer>
+
+
+
     </>
-);
+  );
 };
 
 export default Contactus;
