cc791ba7ac6cd8b47c0d09222c93345185e1437e
Ramesh Pathak <cpathakramesh123@gmail.com>
2022-06-10 10:29:50 +0545

    Add Diff Price with Diff Services

--- src/Components/Products/AdminViewProducts.js
+++ src/Components/Products/AdminViewProducts.js
@@ -3,10 +3,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 // import AdminDashboard from "../Admin/AdminDashboard"
 
-
 // use reducer
 
-const AdminProducts = ({adminData}) => {
+const AdminProducts = ({ adminData }) => {
   const [pdata, setProductData] = useState([]);
   const [noti, setNoti] = useState([]);
 
@@ -22,7 +21,7 @@ const AdminProducts = ({adminData}) => {
       .catch((err) => {
         console.log(err);
       });
-      axios
+    axios
       .get("http://localhost:5000/service/all-noti")
       .then((response) => {
         if (response) {
@@ -49,7 +48,6 @@ const AdminProducts = ({adminData}) => {
     console.log(pid);
   };
 
- 
   const logoutHandle = () => {
     localStorage.clear();
     window.location = "/";
@@ -65,8 +63,7 @@ const AdminProducts = ({adminData}) => {
 
   return (
     <>
-
-<div className="container-fluid ps-0 py-3">
+      <div className="container-fluid ps-0 py-3">
         <div className="">
           <div className="row">
             <div className="col-md-3">
@@ -258,82 +255,84 @@ const AdminProducts = ({adminData}) => {
             </div>
           </div>
           <div className="col-md-9">
+            {/* implementing conditions for products in cart */}
+            {pdata.length === 0 ? (
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-10 col-12">
+                    <Link to="/addProduct" className="btn btn-info">
+                      Add Product
+                    </Link>
+                    <h3 className="m-5">
+                      There is no any products to display!!!
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <>
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="row justify-content-center">
+                        <div className="col-md-8 col-12">
+                          <div className="card my-5">
+                            <div className="card-body">
+                              <table class="table table-responsive">
+                                <thead>
+                                  <tr>
+                                    {headers.map((row) => {
+                                      return <th key={row.key}>{row.label}</th>;
+                                    })}
+                                  </tr>
+                                </thead>
+                                <tbody style={{ justifyContent: "center" }}>
+                                  {/* for produdct added data  data : use loop*/}
+                                  {pdata.map((items) => {
+                                    return (
+                                      <tr>
+                                        <td>
+                                          <img
+                                            src={
+                                              "http://localhost:5000/" +
+                                              items.pic
+                                            }
+                                            alt=""
+                                            className="img-fluid"
+                                            style={{
+                                              maxWidth: "100px",
+                                              maxHeight: "100px",
+                                              borderRadius: "5px",
+                                            }}
+                                          />
+                                        </td>
 
-            
+                                        <td>
+                                          <span
+                                            style={{
+                                              fontWeight: "600",
+                                              marginLeft: "50px",
+                                            }}
+                                          >
+                                            {items.pname}
+                                          </span>
+                                        </td>
 
-      {/* implementing conditions for products in cart */}
-      {pdata.length === 0 ? (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-10 col-12">
-              <Link to="/addProduct" className="btn btn-info">
-                Add Product
-              </Link>
-              <h3 className="m-5">There is no any products to display!!!</h3>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="row justify-content-center">
-                  <div className="col-md-8 col-12">
-                    <div className="card my-5">
-                      <div className="card-body">
-                        <table class="table table-responsive">
-                          <thead>
-                            <tr>
-                              {headers.map((row) => {
-                                return <th key={row.key}>{row.label}</th>;
-                              })}
-                            </tr>
-                          </thead>
-                          <tbody style={{ justifyContent: "center" }}>
-                            {/* for produdct added data  data : use loop*/}
-                            {pdata.map((items) => {
-                              return (
-                                <tr>
-                                  <td>
-                                    <img
-                                      src={"http://localhost:5000/" + items.pic}
-                                      alt=""
-                                      className="img-fluid"
-                                      style={{
-                                        maxWidth: "100px",
-                                        maxHeight: "100px",
-                                        borderRadius: "5px",
-                                      }}
-                                    />
-                                  </td>
+                                        <td>
+                                          <span
+                                            style={{
+                                              fontWeight: "600",
+                                              marginLeft: "50px",
+                                            }}
+                                          >
+                                            {items.pqty}
+                                          </span>
+                                        </td>
 
-                                  <td>
-                                    <span
-                                      style={{
-                                        fontWeight: "600",
-                                        marginLeft: "50px",
-                                      }}
-                                    >
-                                      {items.pname}
-                                    </span>
-                                  </td>
+                                        <td>{items.pdesc}</td>
 
-                                  <td>
-                                    <span
-                                      style={{
-                                        fontWeight: "600",
-                                        marginLeft: "50px",
-                                      }}
-                                    >
-                                      {items.pqty}
-                                    </span>
-                                  </td>
-
-                                  <td>{items.pdesc}</td>
-
-                                  <td>
-                                    {/* <i
+                                        <td>
+                                          {/* <i
                                           className="bi bi-trash-fill"
                                           onClick={deleteAdminProduct.bind(
                                             this,
@@ -345,38 +344,38 @@ const AdminProducts = ({adminData}) => {
                                             fontSize: "1.2rem",
                                           }}
                                         ></i> */}
-                                    {/* fkjkfj */}
-                                    <Link
-                                      to={`/update-product/${items._id}`}
-                                      className="d-flex"
-                                    >
-                                      <i className="bi bi-pencil-square fa-2x text-primary"></i>
-                                    </Link>
-                                    <span
-                                      onClick={deleteAdminProduct.bind(
-                                        this,
-                                        items._id
-                                      )}
-                                      style={{ cursor: "pointer" }}
-                                      className="d-flex"
-                                    >
-                                      <i className="bi bi-trash-fill fa-2x text-danger"></i>
-                                    </span>
-                                  </td>
-                                </tr>
-                              );
-                            })}
-                          </tbody>
-                        </table>
+                                          {/* fkjkfj */}
+                                          <Link
+                                            to={`/update-product/${items._id}`}
+                                            className="d-flex"
+                                          >
+                                            <i className="bi bi-pencil-square fa-2x text-primary"></i>
+                                          </Link>
+                                          <span
+                                            onClick={deleteAdminProduct.bind(
+                                              this,
+                                              items._id
+                                            )}
+                                            style={{ cursor: "pointer" }}
+                                            className="d-flex"
+                                          >
+                                            <i className="bi bi-trash-fill fa-2x text-danger"></i>
+                                          </span>
+                                        </td>
+                                      </tr>
+                                    );
+                                  })}
+                                </tbody>
+                              </table>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
+              </>
+            )}
           </div>
         </div>
       </div>
